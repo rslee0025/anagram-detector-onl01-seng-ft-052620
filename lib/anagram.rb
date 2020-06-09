724a7c42ad3c50d47ac8c381247cf8071c9a9f17
Rachel S. Lee <rslee0025@gmail.com>
2020-06-09 01:54:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  
+  @@all = []
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(word)
+   if @@all.detect {|word| word == word}
+   else 
+     return @@all
+   end
+  end 
+  
+  
+  
+end 
