4f585aadf8cc96d9dd6279d310898f71df3cd660
Rachel S. Lee <rslee0025@gmail.com>
2020-06-09 02:12:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word, :reverse_array
+
+  def initialize(word)
+    self.word = word.downcase
+    self.reverse_array = self.word.reverse.split("").sort
+  end
+
+  def match(words_array)
+    result = []
+    words_array.each do |candidate|
+      if candidate.split("").sort == self.reverse_array
+        result << candidate
+      end
+    end
+    return result
+  end
+end
+  
+  
